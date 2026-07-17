fc67b5cf68de6cfd3136f36fd3b8733e201b269c
Dicky Aldian Pratama <email.bgdicky123@gmail.com>
2026-07-17 14:32:24 +0700

    Memaksa Vercel menggunakan folder tmp dan mengatasi Error 500

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
-// Forward Vercel requests to normal Laravel public/index.php
+// Force Laravel to use /tmp for views and array for cache on Vercel
+putenv('VIEW_COMPILED_PATH=/tmp');
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp';
+
+putenv('CACHE_STORE=array');
+$_ENV['CACHE_STORE'] = 'array';
+$_SERVER['CACHE_STORE'] = 'array';
+
 require __DIR__ . '/../public/index.php';
