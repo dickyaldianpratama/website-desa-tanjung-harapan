65c5e4d0b0c14fde1878e391d8c4330249e9f32b
Dicky Aldian Pratama <email.bgdicky123@gmail.com>
2026-09-07 17:26:17 +0700

    Remove 'Lihat Semua' link from Aparatur Desa card

--- resources/views/pages/profil.blade.php
+++ resources/views/pages/profil.blade.php
@@ -748,7 +748,6 @@
                         <div style="color:#94a3b8;font-size:.85rem">Perangkat yang melayani masyarakat</div>
                     </div>
                 </div>
-                <a href="#" style="color:var(--coklat-tua);font-weight:600;font-size:.9rem;text-decoration:none">Lihat Semua <i class="bi bi-arrow-right"></i></a>
             </div>
 
             <div class="swiper perangkat-swiper" data-aos="fade-up" data-aos-delay="100">
